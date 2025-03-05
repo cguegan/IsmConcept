ac0a7b8fd884e2fb1c7f739a908d48304c250c61
Christophe Guegan <cguegan@gmail.com>
2025-03-05 11:30:35 +0100

    Initial Commit

---
+++ IsmConcept/IsmConceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  IsmConceptApp.swift
+//  IsmConcept
+//
+//  Created by Christophe Guégan on 05/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct IsmConceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
